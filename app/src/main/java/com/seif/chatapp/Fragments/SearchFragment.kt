77d6a5052b65ed489eee4d6a1d02243232ddba20
seif <seifeldinmohamed101@gmail.com>
2021-02-06 01:50:53 +0200

    ChatMe

--- app/src/main/java/com/seif/chatapp/Fragments/SearchFragment.kt
+++ app/src/main/java/com/seif/chatapp/Fragments/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.seif.chatapp.Fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -26,31 +27,31 @@ import com.seif.chatapp.R
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
-    private var userAdapter : UserAdapter? = null
-    private var mUser : List<user>? = null
-    private var recyclerView:RecyclerView? = null
-    private var searchedittext:EditText? = null
+    private var userAdapter: UserAdapter? = null
+    private var mUser: List<user>? = null
+    private var recyclerView: RecyclerView? = null
+    private var searchedittext: EditText? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        val view : View = inflater.inflate(R.layout.fragment_search, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_search, container, false)
         recyclerView = view.findViewById(R.id.rec_search)
-       // recyclerView!!.setHasFixedSize(true)
+        recyclerView!!.setHasFixedSize(true)
         recyclerView!!.layoutManager = LinearLayoutManager(context)
         searchedittext = view.findViewById(R.id.edit_search_user)
-        recyclerView!!.addItemDecoration(DividerItemDecoration(context,OrientationHelper.VERTICAL))
-
+        recyclerView!!.addItemDecoration(DividerItemDecoration(context, OrientationHelper.VERTICAL))
 
         mUser = ArrayList()
         retriveAllUsers()
-        searchedittext!!.addTextChangedListener(object : TextWatcher{
+        searchedittext!!.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    searchForUser(s.toString().toLowerCase())
+                searchForUser(s.toString().toLowerCase())
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -64,52 +65,72 @@ class SearchFragment : Fragment() {
         val userId = FirebaseAuth.getInstance().currentUser!!.uid
         val ref = FirebaseDatabase.getInstance().reference.child("users")
 
-        ref.addValueEventListener(object : ValueEventListener{
+        ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if (edit_search_user==null) return
-                if (edit_search_user.text.toString() == ""){
+                if (edit_search_user == null) return
+                if (edit_search_user.text.toString() == "") {
                     (mUser as ArrayList<user>).clear()
                     snapshot.children.forEach {
                         val user = it.getValue(user::class.java)
-                        if (user!=null && user.uid != userId){
+                        if (user != null && user.uid != userId) {
                             (mUser as ArrayList<user>).add(user)
                         }
                     }
-                    userAdapter = UserAdapter(context!!,mUser!!,false)
+                    userAdapter = UserAdapter(context!!, mUser!!, false)
                     recyclerView!!.adapter = userAdapter
-                    recyclerView!!.addItemDecoration(DividerItemDecoration(context!!,OrientationHelper.VERTICAL))
+                    recyclerView!!.addItemDecoration(
+                        DividerItemDecoration(
+                            context!!,
+                            OrientationHelper.VERTICAL
+                        )
+                    )
                 }
             }
+
             override fun onCancelled(error: DatabaseError) {
 
             }
         })
-
     }
-    private fun searchForUser(str:String){
+
+    private fun searchForUser(str: String) {
         val userId = FirebaseAuth.getInstance().currentUser!!.uid
         val queryUsers = FirebaseDatabase.getInstance().reference
             .child("users").orderByChild("search")
             .startAt(str).endAt(str + "\uf8ff")
-        queryUsers.addValueEventListener(object :ValueEventListener{
+        queryUsers.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                (mUser as ArrayList<user>).clear()
-                snapshot.children.forEach {
-                    var user = it.getValue(user::class.java)
-                    if (user != null && user.uid != userId) {
-                        (mUser as ArrayList<user>).add(user)
+                if (snapshot.exists()) {
+                    (mUser as ArrayList<user>).clear()
+                    snapshot.children.forEach {
+                        val user = it.getValue(user::class.java)
+                        if (user != null && user.uid != userId) {
+                            (mUser as ArrayList<user>).add(user)
+                        }
                     }
-                }
-                userAdapter = UserAdapter(context!!,mUser!!,false)
-                recyclerView!!.adapter = userAdapter
-                recyclerView!!.addItemDecoration(DividerItemDecoration(context!!,OrientationHelper.VERTICAL))
+                    userAdapter = UserAdapter(context, mUser!!, false)
+                    recyclerView!!.adapter = userAdapter
 
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
 
             }
         })
+    }
+
+    private fun updateStatus(status: String) {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val ref = FirebaseDatabase.getInstance().reference.child("users").child(currentUser!!.uid)
+        val hashmap = HashMap<String, Any>()
+        hashmap["status"] = status
+        ref.updateChildren(hashmap)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        updateStatus("online")
 
     }
 
